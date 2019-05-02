f61abcdcd68c9eb0154ea99ec0c95995350c42db
meksey <malyugin.lesha36@gmail.com>
2019-05-02 15:39:09 +0300

    Обновлены ORM модели

--- APP/app/models.py
+++ APP/app/models.py
@@ -11,75 +11,72 @@ class BaseModel(Model):
 class IUser(BaseModel):
     Login = FixedCharField(20)
     Password = FixedCharField(20)
-    FIO = FixedCharField(100)
 
     def exit(self):
         pass
 
 # Интерфейс сущности элемент системы
 class IElement(BaseModel):
-    ID = AutoField(primary_key=True)
+    ID = AutoField()
 
+
+# Абонемент
+class SUBSCRIPTION(IElement):
+    WorkoutsCount = IntegerField(default=30)
+    CompletedWorkouts = IntegerField(default=0)
 
 # Филиал клуба
 class DEPARTMENT(IElement):
     City = FixedCharField(45)
     Address = FixedCharField(250)
 
-# День недели
-class WEEKDAY(IElement):
-    Title = FixedCharField(20)
-
-# Уровни абонемента
-class LEVELS(IElement):
-    Trainings = IntegerField(default=30)
-    LevelName = FixedCharField(30)
-    Price = IntegerField()
-
+    @staticmethod
+    def getDepList():
 
 # Админы
 class ADMIN(IUser, IElement):
-    def monitorCard(self):
-        pass
+    @staticmethod
+    def addSub(client, days):
 
     def getCoaches(self):
         pass
 
 # Клиенты клуба
 class CLIENT(IUser, IElement):
-    BirthDate = DateField(format("%d-%m-%Y"))
-    SubLevel = ForeignKeyField(LEVELS, db_column='ID')
-    SubSrartDate = DateField(format("%d-%m-%Y"), default=datetime.date.today())
-    TrainingsCount = IntegerField(default=0)
-
+    FIO = FixedCharField(100)
+    Sub_ID = ForeignKeyField(
+        SUBSCRIPTION,
+        db_column='Sub_ID'
+    )
     def recording(self):
         pass
 
 # Тренера
 class COACH(IUser, IElement):
-    BirthDate = DateField(format("%d-%m-%Y"))
-    Price = IntegerField()
-    Dep_ID = ForeignKeyField(
+    FIO = FixedCharField(100)
+    Dep = ForeignKeyField(
         DEPARTMENT,
-        db_column='ID'
+        db_column='Dep'
     )
-
     def viewShedule(self):
         pass
-    def changePrice(self):
-        pass
-
-
 
 # Активности
 class ACTIVITY(IElement):
     Title = FixedCharField(45)
     Venue_Title = FixedCharField(45)
 
+    @staticmethod
+    def getActivities():
+        actlist = []
+        for el in ACTIVITY.select():
+            actlist.append((str(el.ID), el.Title))
+        return actlist
+
 # Активность тренера
 class COACH_ACTIVITY(BaseModel):
-    Coach_ID = ForeignKeyField(COACH, db_column='ID')
-    Activity_ID = ForeignKeyField(ACTIVITY, db_column='ID')
+    Coach_ID = ForeignKeyField(COACH, db_column='Coach_ID')
+    Activity_ID = ForeignKeyField(ACTIVITY, db_column='Activity_ID')
 
     class Meta:
         primary_key = CompositeKey('Coach_ID','Activity_ID')
@@ -87,15 +84,6 @@ class COACH_ACTIVITY(BaseModel):
 # Тренировки
 class TRAINING(IElement):
     Start_time = DateTimeField()
-    Date = DateField(format("%d-%m-%Y"))
     Client_ID = ForeignKeyField(CLIENT, db_column='ID')
     Coach_ID = ForeignKeyField(COACH, db_column='ID')
     Activity_ID = ForeignKeyField(ACTIVITY, db_column='ID')
-
-# Рабочий день тренера
-class WORK_DAY(BaseModel):
-    Coach_ID = ForeignKeyField(COACH, db_column='ID')
-    Weekday_ID = ForeignKeyField(WEEKDAY, db_column='ID')
-
-    class Meta:
-        primary_key = CompositeKey('Coach_ID', 'Weekday_ID')
